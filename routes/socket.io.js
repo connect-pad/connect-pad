cd1998c358db9c205d16166dbd269ff149bd0905
Kim Hanseulmaro <hanseulmaro.kim@ciceron.me>
2017-12-16 02:31:39 +0900

    연습

---
+++ routes/socket.io.js
@@ -0,0 +1,15 @@
+module.exports = (io) => {
+	io.on('connection', (socket) => { // 웹소켓 연결 시
+		var uid  = Math.random();
+
+		console.log('Socket initiated!');
+		socket.on('newScoreToServer', (data) => {
+			console.log('Socket: newScore');
+			io.emit('newScoreToClient', { message: "반가워" });
+		});
+		socket.on('init', function(data){
+			
+		});
+	});
+
+};
