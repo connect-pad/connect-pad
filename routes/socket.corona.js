3ea69a3b052608ade02cbb95d3ea4408d896ac51
Kim Hanseulmaro <honeymaro@live.co.kr>
2017-12-16 10:49:23 +0900

    멀라

--- routes/socket.corona.js
+++ routes/socket.corona.js
@@ -42,39 +42,36 @@ wsServer.on('request', function (request) {
 	// accept connection - you should check 'request.origin' to
 	// make sure that client is connecting from your website
 	// (http://en.wikipedia.org/wiki/Same_origin_policy)
-	var connection = request.accept(null, request.origin);
+	global.connection = request.accept(null, request.origin);
 	// we need to know client index to remove them on 'close' event
-	var index = clients.push(connection) - 1;
-	var userName = false;
-	var userColor = false;
 	console.log((new Date()) + ' Connection accepted.');
 	// send back chat history
-	if (history.length > 0) {
-		connection.sendUTF(
-			JSON.stringify({ type: 'history', data: history }));
-	}
+	// if (history.length > 0) {
+	// 	global.connection.sendUTF(
+	// 		JSON.stringify({ type: 'history', data: history }));
+	// }
 	// user sent some message
-	connection.on('message', function (message) {
+	global.connection.on('message', function (message) {
 		if (message.type === 'utf8') { // accept only text
 			// first message sent by user is their name
 
 			// message.utf8Data
 
-			// connection.sendUTF(
+			// global.connection.sendUTF(
 			// 	JSON.stringify({ type: 'color', data: userColor }));
 			// console.log((new Date()) + ' User is known as: ' + userName
 			// 	+ ' with ' + userColor + ' color.');
 		}
 	});
 	// user disconnected
-	connection.on('close', function (connection) {
-		if (userName !== false && userColor !== false) {
-			console.log((new Date()) + " Peer "
-				+ connection.remoteAddress + " disconnected.");
-			// remove user from the list of connected clients
-			clients.splice(index, 1);
-			// push back user's color to be reused by another user
-			colors.push(userColor);
-		}
+	global.connection.on('close', function (connection) {
+		// if (userName !== false && userColor !== false) {
+		// 	console.log((new Date()) + " Peer "
+		// 		+ connection.remoteAddress + " disconnected.");
+		// 	// remove user from the list of connected clients
+		// 	clients.splice(index, 1);
+		// 	// push back user's color to be reused by another user
+		// 	colors.push(userColor);
+		// }
 	});
 });
--- routes/socket.io.js
+++ routes/socket.io.js
@@ -1,4 +1,5 @@
 const md5 = require('md5');
+
 module.exports = (io) => {
 
 	io.on('connection', (socket) => { // 웹소켓 연결 시
@@ -16,21 +17,58 @@ module.exports = (io) => {
 
 		socket.on('arrowKeyDown', (data) => {
 			console.log(uid + " " + JSON.stringify(data));
+			global.connection.sendBytes(
+				Buffer.from(
+					JSON.stringify({
+						event: "arrowKeyDown",
+						uid: uid,
+						arrow: data.arrow
+					}), 'utf8')
+			);
 		});
 		socket.on('arrowKeyUp', (data) => {
 			console.log(uid + " " + JSON.stringify(data));
+			global.connection.sendBytes(
+				Buffer.from(
+					JSON.stringify({
+						event: "arrowKeyUp",
+						uid: uid
+					}), 'utf8')
+			);
 		});
 
 		socket.on('rotationKeyDown', (data) => {
 			console.log(uid + " " + JSON.stringify(data));
+			global.connection.sendBytes(
+				Buffer.from(
+					JSON.stringify({
+						event: "rotationKeyDown",
+						uid: uid,
+						angle: data.angle
+					}), 'utf8')
+			);
 		});
 		socket.on('rotationKeyUp', (data) => {
 			console.log(uid + " " + JSON.stringify(data));
+			global.connection.sendBytes(
+				Buffer.from(
+					JSON.stringify({
+						event: "rotationKeyUp",
+						uid: uid
+					}), 'utf8')
+			);
 		});
 
 		socket.on('disconnect', function () {
 			global.userList = global.userList.filter(usr => usr.connected);
 			console.log(uid + " disconnected");
+			global.connection.sendBytes(
+				Buffer.from(
+					JSON.stringify({
+						event: "userDisconnected",
+						uid: uid
+					}), 'utf8')
+			);
 		});
 	});
 
